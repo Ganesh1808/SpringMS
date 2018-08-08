da8e2b6c0317187d13fafe8aac779ee98010e5c4
Ganesh1808 <ganeshrrrr18@gmail.com>
2018-08-08 13:43:43 +0530

    .

---
+++ src/main/java/com/training/limitconfigserver/LimitconfigserverApplication.java
@@ -0,0 +1,12 @@
+package com.training.limitconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimitconfigserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LimitconfigserverApplication.class, args);
+	}
+}
